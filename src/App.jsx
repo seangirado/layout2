e017fa2f6255f2e3b893b40dbef0177deb38101a
seangirado <seangirado@gmail.com>
2025-09-20 23:42:32 +0800

    latest update

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
-function App() {
 
-  return (
-    <>
-      <div class="container-green">
-        <header class="container-blue-top">
-          <div class="container-light-blue-top">
-            <div class="box box-peach"></div>
-            <div class="box box-red-small-container">
-              <div class="box box-red-small"></div>
-              <div class="box box-red-small"></div>
-            </div>
-            <div class="box box-peach"></div>
-          </div>
-        </header>
-
-        <main class="container-red-large-container">
-          <div class="box box-red-large"></div>
-          <div class="container-red-right-side">
-            <div class="box box-red-medium"></div>
-            <div class="box box-red-medium"></div>
-          </div>
-        </main>
-
-        <footer class="container-blue-bottom">
-          <div class="container-light-blue-bottom">
-            <div class="box box-light-purple"></div>
-            <div class="box box-peach-small"></div>
-            <div class="box box-peach-small"></div>
-            <div class="box box-light-purple"></div>
-          </div>
-        </footer>
-      </div>
-    </>
-  )
+function TopSideBox() {
+  return <div className="box box-peach"></div>;
 }
 
-export default App
+function TopRedBoxContent(props) {
+  return <div className="box box-red-small">{props.name}</div>;
+}
+
+function TopRedBoxContainer() {
+  return (
+    <div className="box box-red-small-container">
+      <TopRedBoxContent name="Sean" />
+      <TopRedBoxContent name="Girado" />
+    </div>
+  );
+}
+
+function TopSection() {
+  return (
+    <div className="container-blue-top">
+      <div className="container-light-blue-top">
+        <TopSideBox />
+        <TopRedBoxContainer />
+        <TopSideBox />
+      </div>
+    </div>
+  );
+}
+
+
+function MiddleLargeRedBox() {
+  return <div className="box box-red-large"></div>;
+}
+
+function MiddleRedBoxContent() {
+  return <div className="box box-red-medium"></div>;
+}
+
+function MiddleRightSide() {
+  return (
+    <div className="container-red-right-side">
+      <MiddleRedBoxContent />
+      <MiddleRedBoxContent />
+    </div>
+  );
+}
+
+function MiddleSection() {
+  return (
+    <div className="container-red-large-container">
+      <MiddleLargeRedBox />
+      <MiddleRightSide />
+    </div>
+  );
+}
+
+
+function BottomLightPurpleBox() {
+  return <div className="box box-light-purple"></div>;
+}
+
+function BottomPeachBox(props) {
+  return <div className="box box-peach-small">{props.name}</div>;
+}
+
+function BottomLightBlueContainer() {
+  return (
+    <div className="container-light-blue-bottom">
+      <BottomLightPurpleBox />
+      <BottomPeachBox name="C-PCIT9" />
+      <BottomPeachBox name="BSIT-3A" />
+      <BottomLightPurpleBox />
+    </div>
+  );
+}
+
+function BottomSection() {
+  return (
+    <div className="container-blue-bottom">
+      <BottomLightBlueContainer />
+    </div>
+  );
+}
+
+
+function App() {
+  return (
+    <div className="container-green">
+      <TopSection />
+      <MiddleSection />
+      <BottomSection />
+    </div>
+  );
+}
+
+export default App;
